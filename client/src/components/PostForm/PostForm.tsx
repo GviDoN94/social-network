0a32aaee09f0c87a824240860f9a91fc8b257666
Vladislav Surin <vlad_surin@mail.ru>
2024-02-25 21:24:44 +0300

    Implemented validation on the client for the form for sending a new post

--- client/src/components/PostForm/PostForm.tsx
+++ client/src/components/PostForm/PostForm.tsx
@@ -1,5 +1,8 @@
-import { FC, FormEventHandler, useState } from 'react';
+import { FC } from 'react';
 import { useMutation } from '@tanstack/react-query';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { createPost } from '@/api/Post';
 import { queryClient } from '@/api/queryClient';
@@ -10,12 +13,24 @@ import './PostForm.css';
 
 export interface IPostFormProps {}
 
+const CreatePostSchema = z.object({
+  text: z.string().min(10, 'Длинна поста должна быть не менее 10 символов'),
+});
+
+type CreatePostForm = z.infer<typeof CreatePostSchema>;
+
 export const PostForm: FC<IPostFormProps> = () => {
-  const [text, setText] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreatePostForm>({
+    resolver: zodResolver(CreatePostSchema),
+  });
 
   const createPostMutation = useMutation(
     {
-      mutationFn: () => createPost(text),
+      mutationFn: createPost,
       onSuccess() {
         queryClient.invalidateQueries({ queryKey: ['posts'] });
       },
@@ -23,22 +38,18 @@ export const PostForm: FC<IPostFormProps> = () => {
     queryClient,
   );
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => [
-    event.preventDefault(),
-
-    createPostMutation.mutate(),
-  ];
-
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(({ text }) => createPostMutation.mutate(text))}
       className="post-form"
     >
-      <FormField label="Текст поста">
+      <FormField
+        label="Текст поста"
+        errorMessage={errors.text?.message}
+      >
         <textarea
           className="post-form__input"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          {...register('text')}
         />
       </FormField>
 
